2ab898f6bf8128c9bfb7ea2a49abc17ab0c21be2
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2015-01-30 13:54:08 +0100

    Fix downloader function name
    As the function doesn't 'merge' files, its name doesn't suit.

--- dl.sh
+++ dl.sh
@@ -5,11 +5,11 @@
 ## Download youtube video and sound and merge them into one file named after
 ## the youtube video title.
 
-merge() {
+download() {
  youtube-dl \
   -f bestvideo+bestaudio \
   -o '/downloads/%(uploader)s/%(title)s-%(id)s.%(ext)s' \
   $@
 }
 
-merge $@
+download $@
